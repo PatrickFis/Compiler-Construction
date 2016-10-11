dae2dff6eb3c1fc0fb741f98c25df510d4c5e3db
Patrick Fischer <fischerpl18@gmail.com>
2016-10-11 10:22:16 -0500

    Initial Commit

---
+++ Checkpoint 2/test.s
@@ -0,0 +1,20 @@
+mAiN
+iF
+elsE
+counTing
+#test comment
+-1232
++12321
+2131
+12.231
+.2131
+0.1231
+0.1001E-12
+inTeger
+real:
+!,
+#(test)
+integer: x, y, z;
+print
+print "charstring";
+test asd;flejwaonclskdjncfdsaf;;
