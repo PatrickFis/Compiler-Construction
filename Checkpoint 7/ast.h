10737013b49f3e62812ad81913a5858de51742fd
Patrick Fischer <fischerpl18@gmail.com>
2016-11-08 12:10:45 -0600

    Initial commit for checkpoint 7.

---
+++ Checkpoint 7/ast.h
@@ -0,0 +1,57 @@
+#ifndef ast_h
+#define ast_h
+
+#define KIND_INT 0
+#define KIND_REAL 1
+#define KIND_OP 2
+
+#define TYPE_VAR 3 // This is for parsing exp = varname
+
+#define OP_ASGN 0
+#define OP_ADD 1
+#define OP_SUB 2
+#define OP_MUL 3
+#define OP_DIV 4
+#define OP_UMIN 5
+#define OP_LSTHN 6
+#define OP_LSTHNEQL 7
+#define OP_GRTHN 8
+#define OP_GRTHNEQL 9
+#define OP_EQUAL 10
+#define OP_NEQUAL 11
+#define OP_AND 12
+#define OP_OR 13
+#define OP_NOT 14
+
+struct statement {
+    struct statement *link;
+    struct ast_expression *exp;
+    //struct symbol_table_entry *target;
+};
+
+struct ast_node { // This portion will probably be used for control structures, etc
+    struct ast_node *left;
+    struct ast_node *right;
+};
+
+struct ast_expression { // This portion will just be used for expressions(possibly just integer expressions)
+    char kind;
+    char operator;
+    char type;
+    struct ast_expression *l_operand;
+    struct ast_expression *r_operand;
+    int value;
+    double rvalue;
+    int address;
+    struct symbol_table_entry *target;
+};
+
+void insertStmt(struct statement *stmt);
+//struct ast_expression createExp(char kind, char operator, struct ast_expression l_operand, struct ast_expression r_operand, int value);
+struct ast_expression createExp(char kind, char operator, int value);
+void printList();
+void exprgen(struct ast_expression *exp);
+extern struct statement *list;
+extern struct statement *head; // Pointer to head of the list
+
+#endif /** ast_h */
